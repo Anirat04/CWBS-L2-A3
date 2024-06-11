b1a93b7483c4ef5769fc0172fb3a3fec3d0b5c40
Tanbir Haque <tanbirhaque53@gmail.com>
2024-06-12 01:16:22 +0600

    Password Bycript and remove the password field in response has been added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
+import bcrypt from "bcrypt";
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
+import config from "../../config";
 
 const userSchema = new Schema<TUser>(
   {
@@ -40,5 +42,46 @@ const userSchema = new Schema<TUser>(
     timestamps: true, // Automatically adds createdAt and updatedAt timestamps
   }
 );
+
+userSchema.pre("save", async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this; // doc
+  // hashing password and save into DB
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  next();
+});
+
+// set '' after saving password
+userSchema.post("save", function (doc, next) {
+  // doc.password = "";
+  doc.set("password", undefined, { strict: false });
+  // console.log("NEW console:", doc);
+  next();
+});
+
+// userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+//   return await User.findOne({ id }).select("+password");
+// };
+
+// userSchema.statics.isPasswordMatched = async function (
+//   plainTextPassword,
+//   hashedPassword
+// ) {
+//   return await bcrypt.compare(plainTextPassword, hashedPassword);
+// };
+
+// userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+//   passwordChangedTimestamp: Date,
+//   jwtIssuedTimestamp: number
+// ) {
+//   const passwordChangedTime =
+//     new Date(passwordChangedTimestamp).getTime() / 1000;
+//   return passwordChangedTime > jwtIssuedTimestamp;
+// };
 
 export const User = model<TUser>("User", userSchema);
